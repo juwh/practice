7365a8466e470112b168451920059391a516e464
William H. Ju <william.h.ju@vanderbilt.edu>
2017-10-07 06:52:34 -0500

    Finish rectangle permuations problem

--- point_permutations.cpp
+++ point_permutations.cpp
@@ -5,55 +5,81 @@
  * Given a 2 dimensional point of a rectangle and its area, find permutations of all the other
  * 3 points of the rectangle in 2-D space.
  */
-#include <list>
 #include <cmath>
+#include <iostream>
+#include <list>
 #include <tuple>
 
-class RectPermutations {
-private:
-    struct Point {
-        int x;
-        int y;
+struct Point {
+    int x;
+    int y;
 
-        Point(int x, int y) : x(x), y(y) {}
-        explicit Point(Point p) : x(p.x), y(p.y) {}
-        Point Point::operator+(const Point &other) {
-            return Point{(x + other.x), (y + other.y)};
+    Point(int horz=0, int vert=0) : x(horz), y(vert) {}
+    Point(const Point &p) : x(p.x), y(p.y) {}
+    Point operator+(const Point& a) const
+    {
+        return Point(a.x+x, a.y+y);
+    }
+    std::string toString() {
+        return "(" + std::to_string(x) + "," + std::to_string(y) + ")";
+    }
+};
+
+// function for generating factors
+std::list<Point> listFactors(int area) {
+    int areaRt = (int)std::sqrt(area);
+    std::list<Point> list;
+    for (int i = 1; i <= areaRt; i++) {
+        if (area%i == 0) {
+            list.emplace_back(Point(i, (area/i)));
         }
-    };
-public:
-    // function for generating factors
-    std::list<Point> listFactors(int area) {
-        int sqrt = (int)sqrt(area);
-        std::list<Point> list;
-        for (int i = 1; i <= sqrt; i++) {
-            if (area%i == 0) {
-                list.emplace_back(Point(i, (area/i)));
+    }
+    return list;
+}
+
+std::tuple<Point, Point, Point> otherPoints(Point sp, Point ep) {
+    return std::make_tuple(Point(sp.x, ep.y), ep, Point(ep.x, sp.y));
+};
+
+// function for generating three points for each relative quadrant
+std::list<std::tuple<Point, Point, Point>> rotateRect(Point sp, Point ep) {
+    std::list<std::tuple<Point, Point, Point>> rotations;
+    int xDiff = ep.x - sp.x;
+    int yDiff = ep.y - sp.y;
+    // create tuples of four points, one for each quadrant
+    rotations.emplace_back(otherPoints(sp, ep));
+    rotations.emplace_back(otherPoints(sp, Point(sp.x-xDiff, ep.y)));
+    rotations.emplace_back(otherPoints(sp, Point(ep.x, sp.y-yDiff)));
+    rotations.emplace_back(otherPoints(sp, Point(sp.x-xDiff, sp.y-yDiff)));
+    return rotations;
+};
+
+// primary function for getting all factors for the area and generating permutations
+std::list<std::tuple<Point, Point, Point>> rectPermutations(Point start, int area) {
+    std::list<std::tuple<Point, Point, Point>> permutations;
+    std::list<Point> factors = listFactors(area);
+    for (Point p : factors) {
+        if (p.x != p.y) {
+            auto rotateList = rotateRect(start, start+p);
+            for (auto triplet : rotateList) {
+                permutations.emplace_back(triplet);
             }
         }
-        return list;
+        auto rotateList = rotateRect(start, Point(start.x+p.y, start.y+p.x));
+        for (auto triplet : rotateList) {
+            permutations.emplace_back(triplet);
+        }
     }
-    // function for generating three points for each relative quadrant
-    std::list<std::tuple<Point, Point, Point, Point>> rotateRect(std::list<Point> factors) {
-        std::list<std::tuple<Point, Point, Point, Point>> rotations;
-        // create tuples of four points, one for each quadrant
-        for (Point p : factors) {
-            rotations.push_back(std::tuple(Point(p.x, p.y), Point(-p.x, p.y),
-                                           Point(-p.x, -p.y), Point(p.x, -p.y)));
-        }
-        return rotations;
-    };
-
-    std::tuple<Point, Point, Point> otherPoints(Point sp, Point ep) {
-
-    };
-
-    // primary function for getting all factors for the area and generating permutations
-    std::list<std::tuple<Point, Point, Point>> rectPermutations(Point start, int area) {
-        std::list<std::tuple<Point, Point, Point>> permutations;
-        std::list<Point> factors = listFactors(area);
-        if (start.x != start.y) {
-
-        }
-    };
+    return permutations;
 };
+
+int main() {
+    auto pointList = rectPermutations(Point(1,1), 12);
+    for (const std::tuple<Point, Point, Point>& triplet : pointList) {
+        Point point1 = std::get<0>(triplet);
+        Point point2 = std::get<1>(triplet);
+        Point point3 = std::get<2>(triplet);
+
+        std::cout << point1.toString() << point2.toString() << point3.toString() << std::endl;
+    }
+}
